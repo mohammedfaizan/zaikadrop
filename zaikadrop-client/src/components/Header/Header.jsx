e579862605f054c753e41cf3f555cf8dddef76fc
mohammed faizan <faizan1999.in@gmail.com>
2025-08-07 13:04:57 +0530

    Add zaikadrop-client as normal folder instead of submodule

---
+++ zaikadrop-client/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <div className="header-contents">
+        <h2>Order your favourite food here</h2>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Autem sequi odio minus fugit reprehenderit! Quod, aliquid corrupti voluptatem adipisci maxime quia possimus laboriosam itaque incidunt soluta. Maiores animi accusantium minus.</p>
+        <button>View Menu</button>
+      </div>
+    </div>
+  )
+}
+
+export default Header
