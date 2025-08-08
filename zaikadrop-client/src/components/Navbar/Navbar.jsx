8c96dfccba0e6d17c85b5685585ed609e6c1e49c
mohammed faizan <faizan1999.in@gmail.com>
2025-08-08 20:02:03 +0530

    added signup and login component

--- zaikadrop-client/src/components/Navbar/Navbar.jsx
+++ zaikadrop-client/src/components/Navbar/Navbar.jsx
@@ -2,31 +2,35 @@ import React, { useState } from "react";
 import "./Navbar.css";
 import { assets } from "../../assets/assets.js";
 import Logo from "./../../assets/zaikadrop-logo.png";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({ setShowLogin }) => {
   const [menu, setMenu] = useState("home");
   return (
     <div className="navbar">
       <img src={Logo} alt="" className="logo" />
       <ul className="navbar-menu">
-        <li
+        <Link
+          to="/"
           onClick={() => setMenu("home")}
           className={menu === "home" ? "active" : ""}
         >
           Home
-        </li>
-        <li
+        </Link>
+        <a
+          href="#explore-menu"
           onClick={() => setMenu("menu")}
           className={menu === "menu" ? "active" : ""}
         >
           menu
-        </li>
-        <li
+        </a>
+        <a
+          href="#footer"
           onClick={() => setMenu("contact us")}
           className={menu === "contact us" ? "active" : ""}
         >
           contact us
-        </li>
+        </a>
       </ul>
       <div className="navbar-right">
         <img src={assets.search_icon} alt="" />
@@ -34,7 +38,7 @@ const Navbar = () => {
           <img src={assets.basket_icon} alt="" />
           <div className="dot"></div>
         </div>
-        <button>Sign In</button>
+        <button onClick={() => setShowLogin(true)}>Sign In</button>
       </div>
     </div>
   );
